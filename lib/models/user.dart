d7e41784807acbaf2d88a7df421d608d07c017bb
Chege Brian <cbbryan8@gmail.com>
2021-09-13 22:36:20 +0300

    Add user role to user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,11 +1,13 @@
 class User {
   String? email;
+  String? role;
 
-  User({this.email});
+  User({this.email, this.role});
 
   factory User.fromJson(Map<String, dynamic> responseData) {
     return User(
       email: responseData['email'],
+      role: responseData['role'],
     );
   }
 }
