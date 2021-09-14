aedcd43e569c19a21382fca20d231d15747ba745
Chege Brian <cbbryan8@gmail.com>
2021-09-14 09:38:16 +0300

    Add action cards to admin dashboard

--- lib/ui/screens/dashboard.dart
+++ lib/ui/screens/dashboard.dart
@@ -5,13 +5,51 @@ class AdminDashboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Orientation orientation = MediaQuery.of(context).orientation;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text('Dashboard'),
       ),
-      body: Center(
-        child: Text('Admin side'),
+      body: ListView(
+        children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Text(
+                'Welcome to the admin dashboard.',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+            ),
+          ),
+          GridView.count(
+            crossAxisCount: 2,
+            shrinkWrap: true,
+            padding: const EdgeInsets.all(8.0),
+            children: [
+              GestureDetector(
+                onTap: () {},
+                child: Card(
+                  child: Center(
+                    child: Text('Manage Sperm Banks'),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Card(
+                  child: Center(
+                    child: Text('Add action'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
