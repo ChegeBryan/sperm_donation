27d453f987f4e1c44a61f41c629b0d491ef00e3b
Chege Brian <cbbryan8@gmail.com>
2021-09-14 12:24:09 +0300

    Remove provider on top dahsboard

--- lib/ui/screens/dashboard.dart
+++ lib/ui/screens/dashboard.dart
@@ -1,61 +1,56 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:sperm_donation/services/sperm_bank.dart';
 
 class AdminDashboard extends StatelessWidget {
   const AdminDashboard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Provider(
-      create: (_) => SpermBankProvider(),
-      child: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text('Dashboard'),
-        ),
-        body: ListView(
-          children: [
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Text(
-                  'Welcome to the admin dashboard.',
-                  style: TextStyle(
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.bold,
-                    color: Theme.of(context).primaryColor,
-                  ),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Dashboard'),
+      ),
+      body: ListView(
+        children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Text(
+                'Welcome to the admin dashboard.',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).primaryColor,
                 ),
               ),
             ),
-            GridView.count(
-              crossAxisCount: 2,
-              shrinkWrap: true,
-              padding: const EdgeInsets.all(8.0),
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    Navigator.pushReplacementNamed(context, '/spermBanks');
-                  },
-                  child: Card(
-                    child: Center(
-                      child: Text('Manage Sperm Banks'),
-                    ),
+          ),
+          GridView.count(
+            crossAxisCount: 2,
+            shrinkWrap: true,
+            padding: const EdgeInsets.all(8.0),
+            children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(context, '/spermBanks');
+                },
+                child: Card(
+                  child: Center(
+                    child: Text('Manage Sperm Banks'),
                   ),
                 ),
-                GestureDetector(
-                  onTap: () {},
-                  child: Card(
-                    child: Center(
-                      child: Text('Add action'),
-                    ),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Card(
+                  child: Center(
+                    child: Text('Add action'),
                   ),
                 ),
-              ],
-            ),
-          ],
-        ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
