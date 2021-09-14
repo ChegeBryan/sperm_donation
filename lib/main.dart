bf291b846b6d955b29598a0152e50b6d16f41580
Chege Brian <cbbryan8@gmail.com>
2021-09-14 07:39:34 +0300

    Register admin dashboard to routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sperm_donation/services/auth.dart';
+import 'package:sperm_donation/services/user.dart';
 import 'package:sperm_donation/ui/auth/screens/login.dart';
 import 'package:sperm_donation/ui/auth/screens/register.dart';
+import 'package:sperm_donation/ui/screens/dashboard.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,16 +14,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Sperm Donation',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => AuthProvider()),
+        ChangeNotifierProvider(create: (context) => UserProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Sperm Donation',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: LoginScreen(),
+        routes: {
+          '/login': (context) => LoginScreen(),
+          '/register': (context) => RegisterScreen(),
+          '/dashboard': (context) => AdminDashboard(),
+        },
       ),
-      home: LoginScreen(),
-      routes: {
-        '/login': (context) => LoginScreen(),
-        '/register': (context) => RegisterScreen()
-      },
     );
   }
 }
