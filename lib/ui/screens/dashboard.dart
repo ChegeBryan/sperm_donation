4089f5a950e24babf7065c98ffc56976d6c6ca58
Chege Brian <cbbryan8@gmail.com>
2021-09-14 07:38:49 +0300

    Add initial admin dashboard

---
+++ lib/ui/screens/dashboard.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AdminDashboard extends StatelessWidget {
+  const AdminDashboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Dashboard'),
+      ),
+      body: Center(
+        child: Text('Admin side'),
+      ),
+    );
+  }
+}
